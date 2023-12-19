58b0c540bb1bc102a2b093b84c647b0ac58c78a7
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2023-12-19 07:28:07 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        ServiceCollection serviceCollection = new ();
+        serviceCollection.AddTransient<IManager, OrderManager>()
+            .AddTimeService();
+        ServiceProvider svcProvider = serviceCollection.BuildServiceProvider();
+        
+
+        var timeService = svcProvider.GetService<ITimeService>();
+        Console.WriteLine($"Time: {timeService?.GetTime()}");
+
+        ShowServices(serviceCollection);
+    }
+
+
+    static void ShowServices(ServiceCollection svcCollection)
+    {
+        var sb = new StringBuilder();
+        foreach(var service in svcCollection)
+        {
+            sb.Append("Type: ").AppendLine(service.ServiceType.FullName)
+            .Append("LifeTyme: ").AppendLine(service.Lifetime.ToString())
+            .Append("ImplementationType: ").AppendLine(service.ImplementationType?.FullName)
+            .AppendLine();
+        }
+        Console.WriteLine(sb.ToString());
+    }
+}
+
+
+public interface IManager
+{
+
+}
+
+
+public class OrderManager : IManager
+{
+    
+}
+
+
+interface ITimeService
+{
+    string GetTime();
+}
+
+public static class serviceProciderExtensions
+{
+    public static void AddTimeService(this IServiceCollection services)
+    {
+        services.AddTransient<TimeService>();
+    }
+}
+
+class TimeService : ITimeService
+{
+    public string GetTime() => DateTime.Now.ToShortTimeString();
+}
+
+class LongTimeService : ITimeService
+{
+    public string GetTime() => DateTime.Now.ToLongTimeString();
+}
